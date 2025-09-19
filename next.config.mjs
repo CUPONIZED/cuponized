a2ca21228dc93fb65681cd1befe07084dd00a8aa
CUPONIZED <martinianonunez@gmail.com>
2025-09-19 12:53:36 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,3 @@
-// next.config.mjs
 import createNextIntlPlugin from 'next-intl/plugin';
-
-// Le pasamos dónde está la config de request (paso 2)
 const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true
-};
-
-export default withNextIntl(nextConfig);
+export default withNextIntl({});
