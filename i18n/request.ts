6f3652f9e739f65425a1f78d43c46f824f3d3d13
CUPONIZED <martinianonunez@gmail.com>
2025-09-19 12:52:42 -0300

    Update request.ts

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,13 +1,10 @@
 // i18n/request.ts
 import {getRequestConfig} from 'next-intl/server';
-import {locales, defaultLocale} from './i18n';
+import {locales, defaultLocale} from '../i18n';
 
 export default getRequestConfig(async ({locale}) => {
-  // Normaliza por si llega un locale raro
   const loc = (locales as readonly string[]).includes(locale) ? locale : defaultLocale;
-
   return {
-    // Carga el JSON correspondiente desde /messages
     messages: (await import(`../messages/${loc}.json`)).default
   };
 });
