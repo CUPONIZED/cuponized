79ed669e8c19dd95d74aede154850fbd25a115fd
CUPONIZED <martinianonunez@gmail.com>
2025-09-19 12:26:28 -0300

    Update messages.ts

--- app/[locale]/messages.ts
+++ app/[locale]/messages.ts
@@ -1,8 +1,1 @@
-import {Locale} from "../../i18n";
 
-export async function loadMessages(locale: Locale) {
-  const messages = await import(`../../messages/${locale}.json`).then(
-    (m) => m.default
-  );
-  return messages;
-}
